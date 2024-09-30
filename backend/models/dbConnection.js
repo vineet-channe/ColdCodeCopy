627e5380c127737f8fb960f5008984f1df7c5570
lokesh chaudhari <lokeshchaudhari14@gmail.com>
2024-09-30 13:31:56 +0530

    fixed backend

--- backend/models/dbConnection.js
+++ backend/models/dbConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URL = process.env.DB_URL;
+const DB_URL = 'mongodb+srv://lokesh:lokcode18@cluster0.2lesf.mongodb.net/coldcode?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(DB_URL)
   .then(() => {
